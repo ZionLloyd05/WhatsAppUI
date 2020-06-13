5b937d4164527cff3046517d885d785689551ba9
Alagbala Damilola <zionlloyd05@gmail.com>
2020-06-13 12:13:00 +0100

    initial commit

---
+++ lib/models/chat.dart
@@ -0,0 +1,65 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: 'Alagbala Damilola',
+      message: 'Hey Fluter, You are amazing',
+      time: '15:30',
+      avatarUrl:
+          'https://s3-us-west-2.amazonaws.com/agora-profile-portraits-prod/profile-portraits/prod/persons/986579662010982400/portraits/1253267258121777152/original?AWSAccessKeyId=AKIAJS5XDM2QDHERYJMQ&Expires=2147483647&Signature=G42E2E%2FyaCqbh3DTwVGd4%2FoLQ%2Bk%3D'),
+  new ChatModel(
+      name: 'Harvey Spectre',
+      message: 'I\'m using whatsapp',
+      time: '15:30',
+      avatarUrl:
+          'https://s3-us-west-2.amazonaws.com/agora-profile-portraits-prod/profile-portraits/prod/persons/986579662010982400/portraits/1253267258121777152/original?AWSAccessKeyId=AKIAJS5XDM2QDHERYJMQ&Expires=2147483647&Signature=G42E2E%2FyaCqbh3DTwVGd4%2FoLQ%2Bk%3D'),
+  new ChatModel(
+      name: 'Dave Stone',
+      message: 'How are you doing, hello',
+      time: '15:30',
+      avatarUrl:
+          'https://s3-us-west-2.amazonaws.com/agora-profile-portraits-prod/profile-portraits/prod/persons/986579662010982400/portraits/1253267258121777152/original?AWSAccessKeyId=AKIAJS5XDM2QDHERYJMQ&Expires=2147483647&Signature=G42E2E%2FyaCqbh3DTwVGd4%2FoLQ%2Bk%3D'),
+  new ChatModel(
+      name: 'Joe West',
+      message: 'Hey Fluter, You are amazing',
+      time: '15:30',
+      avatarUrl:
+          'https://s3-us-west-2.amazonaws.com/agora-profile-portraits-prod/profile-portraits/prod/persons/986579662010982400/portraits/1253267258121777152/original?AWSAccessKeyId=AKIAJS5XDM2QDHERYJMQ&Expires=2147483647&Signature=G42E2E%2FyaCqbh3DTwVGd4%2FoLQ%2Bk%3D'),
+  new ChatModel(
+      name: 'Faith Alagbala',
+      message: 'Hey Fluter, You are amazing',
+      time: '15:30',
+      avatarUrl:
+          'https://s3-us-west-2.amazonaws.com/agora-profile-portraits-prod/profile-portraits/prod/persons/986579662010982400/portraits/1253267258121777152/original?AWSAccessKeyId=AKIAJS5XDM2QDHERYJMQ&Expires=2147483647&Signature=G42E2E%2FyaCqbh3DTwVGd4%2FoLQ%2Bk%3D'),
+  new ChatModel(
+      name: 'BBD ^_^',
+      message: 'Very Unavailable',
+      time: '15:30',
+      avatarUrl:
+          'https://s3-us-west-2.amazonaws.com/agora-profile-portraits-prod/profile-portraits/prod/persons/986579662010982400/portraits/1253267258121777152/original?AWSAccessKeyId=AKIAJS5XDM2QDHERYJMQ&Expires=2147483647&Signature=G42E2E%2FyaCqbh3DTwVGd4%2FoLQ%2Bk%3D'),
+  new ChatModel(
+      name: 'Lilian Jones',
+      message: 'Hey Fluter, You are amazing',
+      time: '15:30',
+      avatarUrl:
+          'https://s3-us-west-2.amazonaws.com/agora-profile-portraits-prod/profile-portraits/prod/persons/986579662010982400/portraits/1253267258121777152/original?AWSAccessKeyId=AKIAJS5XDM2QDHERYJMQ&Expires=2147483647&Signature=G42E2E%2FyaCqbh3DTwVGd4%2FoLQ%2Bk%3D'),
+  new ChatModel(
+      name: 'Richie Jr',
+      message: 'Hey Fluter, You are amazing',
+      time: '15:30',
+      avatarUrl:
+          'https://s3-us-west-2.amazonaws.com/agora-profile-portraits-prod/profile-portraits/prod/persons/986579662010982400/portraits/1253267258121777152/original?AWSAccessKeyId=AKIAJS5XDM2QDHERYJMQ&Expires=2147483647&Signature=G42E2E%2FyaCqbh3DTwVGd4%2FoLQ%2Bk%3D'),
+  new ChatModel(
+      name: 'Charlley Bee',
+      message: 'Hey Fluter, You are amazing',
+      time: '15:30',
+      avatarUrl:
+          'https://s3-us-west-2.amazonaws.com/agora-profile-portraits-prod/profile-portraits/prod/persons/986579662010982400/portraits/1253267258121777152/original?AWSAccessKeyId=AKIAJS5XDM2QDHERYJMQ&Expires=2147483647&Signature=G42E2E%2FyaCqbh3DTwVGd4%2FoLQ%2Bk%3D')
+];
